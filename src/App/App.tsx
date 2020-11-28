b97989b28a0bc1936eaee2e1bec3ee322d1b32ee
Eddy Rivera <eddyrivera@ITs-MacBook-Pro.local>
2020-11-28 10:43:43 -0300

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from "react";
+import styles from "./App.module.scss";
+
+const App = (): ReactElement => {
+  return <div className={styles.app}>Hola</div>;
+};
+
+export default App;
